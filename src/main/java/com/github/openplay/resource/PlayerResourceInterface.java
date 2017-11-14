6be0282563b8f1fd7f9f2f0b4addadd7e2749456
elimv <31315134+elimv@users.noreply.github.com>
2017-11-14 16:44:44 -0300

    get para jsp
    agregue un get para ver el jsp

--- src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
+++ src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
@@ -4,7 +4,10 @@ import java.text.ParseException;
 
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.server.mvc.Viewable;
+
 public interface PlayerResourceInterface {
 	public Response registerCampaign(String idUser, String idCampaign) throws ParseException;
-
+	public Response registerCampaign();
+	
 }
