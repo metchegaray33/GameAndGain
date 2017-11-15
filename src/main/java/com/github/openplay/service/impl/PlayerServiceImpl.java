88889c26ec7a6640eac0d722b3b9ddbb8e726219
elimv <31315134+elimv@users.noreply.github.com>
2017-11-15 19:23:55 -0300

    cambios

--- src/main/java/com/github/openplay/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/github/openplay/service/impl/PlayerServiceImpl.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.openplay.model.CampaingsHasUsersInterface;
+import com.github.openplay.model.UsersReceivesBadgesInterface;
 import com.github.openplay.model.impl.CampaignsHasUsers;
+import com.github.openplay.model.impl.UsersReceivesBadges;
 import com.github.openplay.repository.CampaignsHasUsersRepository;
+import com.github.openplay.repository.UsersReceivesBadgesRepository;
 import com.github.openplay.service.PlayerService;
 
 
@@ -16,9 +19,25 @@ public class PlayerServiceImpl implements PlayerService{
 	@Autowired
 	private CampaignsHasUsersRepository campaignsHasUsersRepository;
 	
+	@Autowired
+	private UsersReceivesBadgesRepository usersReceivesBadgesRepository;
+	
+//	@Autowired
+//	private BadgeRepository badgeRepository;
+	
 	@Transactional
 	public CampaingsHasUsersInterface save(CampaingsHasUsersInterface campaign) {
 		return campaignsHasUsersRepository.save((CampaignsHasUsers) campaign);
 	}
+		
+	@Transactional
+	public UsersReceivesBadgesInterface save(UsersReceivesBadgesInterface badge) {
+		return usersReceivesBadgesRepository.save((UsersReceivesBadges) badge);
+	}
 	
+//	public int findByBadgeId(int value){
+//		BadgeInterface badge = badgeRepository.findByBadgeId(findByBadgeId);
+//		return badgeRepository.save
+//	}
+//	
 }
