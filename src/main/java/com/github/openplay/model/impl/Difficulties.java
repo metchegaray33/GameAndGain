678157b6bdb5a6de04e77c10e37de03d519ac784
Tomas Vukasovic <8014547+tvukasovic@users.noreply.github.com>
2017-11-14 23:40:10 -0300

    Made changes on models
    Due to services implementation passed attributes from private to public

--- src/main/java/com/github/openplay/model/impl/Difficulties.java
+++ src/main/java/com/github/openplay/model/impl/Difficulties.java
@@ -1,26 +1,14 @@
 package com.github.openplay.model.impl;
 
-import java.util.Date;
 import java.util.HashSet;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
-
 import com.github.openplay.model.DifficultiesInterface;
-
-
-
-
-import java.io.Serializable;
 import java.util.Set;
-
 import javax.persistence.*;
 
 @Component
@@ -32,7 +20,7 @@ public class Difficulties implements DifficultiesInterface {
 	@Id
 	@GeneratedValue
 	@Column(name="difficultyId")
-	private int difficultyId;
+	public int difficultyId;
 	
 	@NotEmpty
 	@Size(min=4, max=70)
