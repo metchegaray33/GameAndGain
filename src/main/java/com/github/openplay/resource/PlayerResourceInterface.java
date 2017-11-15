55944d8eda558392aa5172d5f4f6848a9019a3b9
elimv <31315134+elimv@users.noreply.github.com>
2017-11-15 19:23:01 -0300

    cambios

--- src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
+++ src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
@@ -4,10 +4,13 @@ import java.text.ParseException;
 
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.server.mvc.Viewable;
-
 public interface PlayerResourceInterface {
 	public Response registerCampaign(String idUser, String idCampaign) throws ParseException;
 	public Response registerCampaign();
+
+	public Response userBadges();
+	public Response userBadges(String idUser, String idBadge, Integer value) throws ParseException;
 	
+//	public Response updateScore();
+//	public Response updateScore(Integer campaigns_Score) throws ParseException;
 }
