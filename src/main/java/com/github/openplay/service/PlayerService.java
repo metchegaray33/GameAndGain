8dc245fc1585f817cd3ab5f05579072430ae12d7
elimv <31315134+elimv@users.noreply.github.com>
2017-11-15 19:24:22 -0300

    cambios

--- src/main/java/com/github/openplay/service/PlayerService.java
+++ src/main/java/com/github/openplay/service/PlayerService.java
@@ -1,7 +1,10 @@
 package com.github.openplay.service;
 
 import com.github.openplay.model.CampaingsHasUsersInterface;
+import com.github.openplay.model.UsersReceivesBadgesInterface;
 
 public interface PlayerService {
 	CampaingsHasUsersInterface save(CampaingsHasUsersInterface campaign);
+	
+	UsersReceivesBadgesInterface save(UsersReceivesBadgesInterface badge);
 }
