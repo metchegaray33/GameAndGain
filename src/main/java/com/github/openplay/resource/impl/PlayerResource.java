909acfe6da8f5ac81b4f52cf371c7c2984600684
elimv <31315134+elimv@users.noreply.github.com>
2017-11-15 19:23:22 -0300

    cambios

--- src/main/java/com/github/openplay/resource/impl/PlayerResource.java
+++ src/main/java/com/github/openplay/resource/impl/PlayerResource.java
@@ -1,7 +1,6 @@
 package com.github.openplay.resource.impl;
 
 import java.text.ParseException;
-import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -18,10 +17,11 @@ import org.glassfish.jersey.server.mvc.Viewable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.github.openplay.model.impl.Badge;
 import com.github.openplay.model.impl.Campaign;
 import com.github.openplay.model.impl.CampaignsHasUsers;
-import com.github.openplay.model.impl.Comment;
 import com.github.openplay.model.impl.User;
+import com.github.openplay.model.impl.UsersReceivesBadges;
 import com.github.openplay.resource.PlayerResourceInterface;
 import com.github.openplay.service.PlayerService;
 
@@ -71,6 +71,70 @@ public class PlayerResource implements PlayerResourceInterface{
 		return Response.ok().entity(new Viewable("/login")).build();
 	}
 	
+	@GET
+	@Path("userBadges")
+	@Produces(MediaType.TEXT_HTML)
+	public Response userBadges() {
+		return Response.ok(new Viewable("/temporalBadgeUser")).build();
+	}
 	
+	@POST
+	@Path("userBadges")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public Response userBadges(
+			@FormParam("idUser") String idUser ,
+			@FormParam("idBadge") String idBadge,
+			@FormParam("value")Integer value)
+			throws ParseException {
 
+		if (idUser == null || idBadge == null || value == null) {
+			return Response.status(Status.PRECONDITION_FAILED).build();
+		}
+		
+		Integer idUserint = Integer.parseInt (idUser);
+		Integer idBadgeInt = Integer.parseInt(idBadge);
+		
+		Badge badge = new Badge();
+		badge.setBadgeId(idBadgeInt.intValue());
+		
+		User user = new User();
+		user.setUserId(idUserint.intValue());
+		
+		UsersReceivesBadges usersReceivesBadges = new UsersReceivesBadges();
+		usersReceivesBadges.setBadge(badge);
+		usersReceivesBadges.setUser(user);
+		usersReceivesBadges.setValue(value);
+		
+		playerService.save(usersReceivesBadges);
+		
+		return Response.ok().entity(new Viewable("/temporalBadgeUser")).build();
+	}
+	
+//	@GET
+//	@Path("updateScore")
+//	@Produces(MediaType.TEXT_HTML)
+//	public Response updateScore() {
+//		return Response.ok(new Viewable("/temporalUpdateScore")).build();
+//	}
+//	
+//	@POST
+//	@Path("updateScore")
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	@Produces(MediaType.TEXT_HTML)
+//	public Response updateScore(
+//			@FormParam("campaigns_Score") Integer campaigns_Score)
+//			throws ParseException {
+//
+//		if (campaigns_Score == null) {
+//			return Response.status(Status.PRECONDITION_FAILED).build();
+//		}
+//		
+//		CampaignsHasUsers campaignsHasUsers = new CampaignsHasUsers();
+//		campaignsHasUsers.setCampaigns_Score(campaigns_Score);
+//		
+//		playerService.save(campaignsHasUsers);
+//		
+//		return Response.ok().entity(new Viewable("/temporalUpdateScore")).build();
+//	}
 }
