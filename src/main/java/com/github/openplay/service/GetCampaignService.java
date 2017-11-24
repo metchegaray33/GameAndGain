86eb9431e202340434d55639ed9152a36e16212b
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-24 17:05:08 -0300

    service consultar campañas funcionando

---
+++ src/main/java/com/github/openplay/service/GetCampaignService.java
@@ -0,0 +1,9 @@
+package com.github.openplay.service;
+
+import com.github.openplay.model.impl.Campaign;
+
+public interface GetCampaignService {
+	
+	public Campaign getCampaignId(String campagin);
+
+}
